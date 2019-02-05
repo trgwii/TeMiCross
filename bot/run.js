440725117167f13885295e9511b9b6b4bdd57bc9
Thomas Rory Gummerson <thomas@gummerson.no>
2019-02-06 00:25:44 +0100

    bugfix

--- bot/run.js
+++ bot/run.js
@@ -75,7 +75,9 @@ const captionMedia = (name, fn) => R.ifElse(
 
 const run = opts => {
 
-	const { token, chatID: tgID, allowList } = opts;
+	const { token, chatID: tgIDStr, allowList } = opts;
+
+	const tgID = Number(tgIDStr);
 
 	const botID = R.head(R.split(':', token));
 
