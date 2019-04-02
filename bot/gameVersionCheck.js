13c87d33519a3c48dff3ee0906daecb8d36ccd93
Thomas Rory Gummerson <thomas@gummerson.no>
2019-04-02 21:52:27 +0200

    Allow snapshot versions and initial update event

--- bot/gameVersionCheck.js
+++ bot/gameVersionCheck.js
@@ -3,21 +3,21 @@
 const { EventEmitter } = require('events');
 const axios = require('axios');
 
-const getVersion = () =>
+const getVersion = (type = 'release') =>
 	axios('https://launchermeta.mojang.com/mc/game/version_manifest.json')
-		.then(x => x.data.latest.release);
+		.then(x => x.data.latest[type]);
 
-const emitUpdates = () => {
+const emitUpdates = (type = 'release', init = false) => {
 	const e = new EventEmitter();
 	let version = '';
 	const check = v => {
-		if (version !== '' && version !== v) {
+		if ((version !== '' || init) && version !== v) {
 			e.emit('update', v);
 		}
 		version = v;
-		setTimeout(() => getVersion().then(check), 10000);
+		setTimeout(() => getVersion(type).then(check), 10000);
 	};
-	getVersion().then(check);
+	getVersion(type).then(check);
 	return e;
 };
 
