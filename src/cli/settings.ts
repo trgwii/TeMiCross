a38d1c05a21710868c96ed39a44b4e16154b082e
Thomas Rory Gummerson <thomas@gummerson.no>
2020-03-02 23:02:15 +0100

    Settings fleshed out

--- src/cli/settings.ts
+++ src/cli/settings.ts
@@ -1,8 +1,33 @@
-export interface Settings {
+import { forEachObjIndexed } from "ramda";
+
+export interface ClientSettings {
+	port: number;
+	ip: string;
+}
+
+export interface WrapperSettings extends ClientSettings {
 	cmd: string;
+}
+
+export interface BotSettings extends ClientSettings {
 	token: string;
 	chatID: string;
-	allowList: boolean;
+	allowList: true;
 	localAuth: boolean;
 	postUpdates: boolean;
 }
+
+export interface BotWrapperSettings {
+	cmd: string;
+	token: string;
+	chatID: string;
+	allowList: true;
+	localAuth: boolean;
+	postUpdates: boolean;
+}
+
+export type Settings =
+	ClientSettings |
+	WrapperSettings |
+	BotSettings |
+	BotWrapperSettings;
